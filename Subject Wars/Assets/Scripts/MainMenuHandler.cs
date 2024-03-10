9fb78a19717c1c4de501024a78595ec304810e4a
NugNug3 <minato.allen@my.utsa.edu>
2024-03-10 18:56:31 -0500

    Main Menu
    Added Basic main menu, and tied the main menu logic to the level start. Title screen design is still a work in progress.

---
+++ Subject Wars/Assets/Scripts/MainMenuHandler.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  //imported unity engine scene manager for changing scenes when clicking buttons
+
+public class MainMenuHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("BaseLevel"); //The scene name is currently "SampleScene" since we have not worked on the level chooser yet. When the level choosing menu is done, SampleScene will be replaced by it.
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Store()
+    {
+        SceneManager.LoadScene("Store"); //This will launch the store scene, which needs to be worked on still.
+    }
+
+}
