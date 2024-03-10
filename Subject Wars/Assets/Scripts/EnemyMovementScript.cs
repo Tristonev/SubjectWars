a6bddfe93908be0f28c5025d5ed6e20228f00ab1
Tristonev <tristonvarela@gmail.com>
2024-03-10 09:56:45 -0500

    Base level additions
    Added collision/triggers and basic enemy spawning

--- Subject Wars/Assets/Scripts/EnemyMovementScript.cs
+++ Subject Wars/Assets/Scripts/EnemyMovementScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitMovementScript : MonoBehaviour
+public class EnemyMovementScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,9 +14,9 @@ public class UnitMovementScript : MonoBehaviour
     void Update()
     {
         float speed = 5;
-        if(transform.position.x < 24)
+        if (transform.position.x > -8)
         {
-            transform.Translate(transform.right * speed * Time.deltaTime);
+            transform.Translate(transform.right * -speed * Time.deltaTime);
         }
     }
 
@@ -29,8 +29,7 @@ public class UnitMovementScript : MonoBehaviour
     void OnTriggerEnter2D(Collider2D col)
     {
         Debug.Log("Trigger Colliders existing collision");
-        Destroy(gameObject);
+        //Destroy(gameObject);
 
     }
-
 }
--- Subject Wars/Assets/Scripts/SpawnerScript.cs
+++ Subject Wars/Assets/Scripts/SpawnerScript.cs
@@ -2,21 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class SpawnerScript : MonoBehaviour
 {
-    public GameObject unitObject;
+    public GameObject addition;
+    public GameObject division;
+    public GameObject pi;
+
+    private float spawnTimer = 4f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(addition, transform.position, Quaternion.identity);
+
+        //Instantiate(division, transform.position, Quaternion.identity);
+
+        //Instantiate(pi, transform.position, Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        AttemptSpawn();
+    }
+
+    void AttemptSpawn()
+    {
+        //This checks to see if the amount of time in spawnTimer has passed
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0)
         {
-            Instantiate(unitObject, transform.position, Quaternion.identity);
+            //Spawns a unit and resets timer
+            Instantiate(addition, transform.position, Quaternion.identity);
+            spawnTimer = 4f;
         }
     }
+
 }
