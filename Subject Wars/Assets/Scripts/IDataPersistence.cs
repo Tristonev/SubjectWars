4dae63ddee01f828eb10405a838d445d5d8731bf
luna782 <paullunapal@gmail.com>
2024-04-27 13:49:19 -0500

    Added Comments
    - IDataPersistence
    - IStatTrack

--- Subject Wars/Assets/Scripts/IDataPersistence.cs
+++ Subject Wars/Assets/Scripts/IDataPersistence.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * IDataPersistence is an interface that is intended to have the methods 
+ * declared below used by an script that implements the interface.
+ */
+
 public interface IDataPersistence
 {
+    //LoadData loads the data from the main copy of the game data to this class
     void LoadData(UserData data);
 
+    //SaveData class saves the data in this class to the main copy of the game data.
     void SaveData(ref UserData data);
 
 }
